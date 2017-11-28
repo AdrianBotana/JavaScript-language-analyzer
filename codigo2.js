a1d46a7b5764a60945a013f07dbe05c749be9727
adrian.botana.perez <adruian@gmail.com>
2017-11-28 12:11:28 +0100

    Initial comit

---
+++ codigo2.js
@@ -0,0 +1,7 @@
+var
+
+/* Programa de ejemplo  */
+/* El ejemplo incorpora elementos del lenguaje opcionales y elementos que no hay que implementar */
+
+var chars s;    /* variable global cadena */
+var int hola;
